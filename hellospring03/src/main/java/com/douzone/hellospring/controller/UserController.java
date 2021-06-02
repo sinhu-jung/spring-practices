fcb7995794acbde70ae00500dc90b301087fd86b
sinhu-jung <tlsgn26@naver.com>
2021-06-02 16:00:13 +0900

    @RequestMapping 연습

---
+++ hellospring03/src/main/java/com/douzone/hellospring/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.douzone.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+	 
+	@RequestMapping(value="/join", method=RequestMethod.GET)
+	public String join() {
+		return "/WEB-INF/views/join.jsp";
+	}
+	
+	@RequestMapping(value="/join", method=RequestMethod.POST)
+	public String join(UserVo vo) {
+		System.out.println(vo);
+		return "redirect:/";
+	}
+}
