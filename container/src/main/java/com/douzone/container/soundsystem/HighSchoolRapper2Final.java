390d75b0a5c816a4361b85a66661d94de8a87665
sinhu-jung <tlsgn26@naver.com>
2021-06-14 09:47:53 +0900

    wiring #2 - 1

---
+++ container/src/main/java/com/douzone/container/soundsystem/HighSchoolRapper2Final.java
@@ -0,0 +1,12 @@
+package com.douzone.container.soundsystem;
+
+public class HighSchoolRapper2Final implements CompactDisc {
+	private String title = "붕붕";
+	private String artist = "김하온";
+	
+	@Override
+	public void play() {
+		System.out.println("Playing" + title + "by" + artist);
+	}
+
+}
---
+++ container/src/test/java/com/douzone/container/config/user/AppConfigTest.java
@@ -0,0 +1,28 @@
+package com.douzone.container.config.user;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.douzone.container.user.User;
+
+public class AppConfigTest {
+	//Java Config
+	//직접 설정 자바 클래스를 전달
+	//설정 클래스에 @Configuration이 필요없다.
+	@Test
+	public void testAppConfig01() {
+		ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig01.class);
+		User user = ac.getBean(User.class);
+		assertNotNull(user);	
+	}
+	
+	@Test
+	public void testAppConfig02() {
+		ApplicationContext ac = new AnnotationConfigApplicationContext("com.douzone.container.config.user");
+		User user = ac.getBean(User.class);
+		assertNotNull(user);
+	}
+}
