d6c253682698d8720560cd8c6de4af50af628512
sinhu-jung <tlsgn26@naver.com>
2021-06-14 11:22:11 +0900

    wiring #2-2

--- container/src/test/java/com/douzone/container/config/user/XmlConfigTest.java
+++ container/src/test/java/com/douzone/container/config/user/XmlConfigTest.java
@@ -8,17 +8,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import com.douzone.container.user.User;
+import com.douzone.container.user.User1;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 //@ContextConfiguration(classes= {AppConfig01.class})
-@ContextConfiguration(locations= {"/com/douzone/container/config/user/applicationContext02.xml"})
+@ContextConfiguration(locations= {"/com/douzone/container/config/user/applicationContext01.xml"})
 public class XmlConfigTest {
 	@Autowired
-	private User user;
+	private User1 user1;
 	
 	@Test
 	public void testUserNotNull() {
-		assertNotNull(user);
+		assertNotNull(user1);
 	}
 }
