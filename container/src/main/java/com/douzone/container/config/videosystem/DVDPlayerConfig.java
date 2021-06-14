1b7316b05956134bbb627fdac33b4f6aadecd86e
sinhu-jung <tlsgn26@naver.com>
2021-06-14 12:49:17 +0900

    explicit wiring - xml #1

--- container/src/main/java/com/douzone/container/config/videosystem/DVDPlayerConfig.java
+++ container/src/main/java/com/douzone/container/config/videosystem/DVDPlayerConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.douzone.container.videosystem.Avengers;
+import com.douzone.container.videosystem.DVDPlayer;
+import com.douzone.container.videosystem.DigitalVideoDisc;
 
 @Configuration
 public class DVDPlayerConfig {
@@ -11,5 +13,39 @@ public class DVDPlayerConfig {
 	@Bean
 	public Avengers avengers() {
 		return new Avengers();
+	}
+	
+	// 주입(Injection) 하기1:
+	// Bean 생성 메소드 직접 호출
+	// 생성자 주입
+	//@Bean
+	public DVDPlayer dvdPlayer() {
+		return new DVDPlayer(avengers());
+	}
+	
+	// 주입(Injection) 하기2:
+	// 파라미터로 Bean 전달하기 
+	// 생성자 주입
+	//@Bean
+	public DVDPlayer dvdPlayer02(DigitalVideoDisc dvd) {
+		return new DVDPlayer(dvd);
+	}
+	
+	// 주입(Injection) 하기3:
+	// 파라미터로 Bean 전달하기 
+	// setter 주입
+	@Bean
+	public DVDPlayer dvdPlayer03(DigitalVideoDisc dvd) {
+		DVDPlayer player = new DVDPlayer();
+		player.setDvd(dvd);
+		return player;
+	}
+	
+	// 같은 타입의 빈을 생성할 경우
+	@Bean(name="player04")
+	public DVDPlayer dvdPlayer04(DigitalVideoDisc dvd) {
+		DVDPlayer player = new DVDPlayer();
+		player.setDvd(dvd);
+		return player;
 	}
 }
--- container/src/test/java/com/douzone/container/config/videosystem/DVDPlayerJavaConfigTest.java
+++ container/src/test/java/com/douzone/container/config/videosystem/DVDPlayerJavaConfigTest.java
@@ -1,5 +1,6 @@
 package com.douzone.container.config.videosystem;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import org.junit.Rule;
@@ -7,6 +8,7 @@ import org.junit.Test;
 import org.junit.contrib.java.lang.system.SystemOutRule;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -23,6 +25,13 @@ public class DVDPlayerJavaConfigTest {
 	private DigitalVideoDisc dvd;
 	
 	@Autowired
+	// 같은 타입의 빈이 2개이상 존재하는 경우1:
+	// 설정 클래스의 빈생성 메소드 이름
+	// @Qualifier("dvdPlayer03")
+	
+	// 같은 타입의 빈이 2개이상 존재하는 경우2:
+	// 설정 클래스의 빈생성 메소드의 @Bean의 name 속성
+	@Qualifier("player04")
 	private DVDPlayer player;
 	
 	@Test
@@ -33,6 +42,12 @@ public class DVDPlayerJavaConfigTest {
 	@Test
 	public void testPlayerNotNull() {
 		assertNotNull(player);
+	}
+	
+	@Test
+	public void testPlay() {
+		player.play();
+		assertEquals("playing Movie MARVEL's Avengers",systemOutRule.getLog().replace("\r\n", "").replace("\n", ""));
 	}
 	
 }
